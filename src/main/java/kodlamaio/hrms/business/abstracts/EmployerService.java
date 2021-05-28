6a85f80b8fe06f88cbc52ba0409f9c695422d608
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-29 02:51:22 +0300

    dataResult integrated and register feature added for employers.

--- src/main/java/kodlamaio/hrms/business/abstracts/EmployerService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/EmployerService.java
@@ -2,8 +2,11 @@ package kodlamaio.hrms.business.abstracts;
 
 import java.util.List;
 
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Employer;
 
 public interface EmployerService {
-	List<Employer> getAll();
+	DataResult<List<Employer>> getAll();
+	Result register(Employer employer);
 }
--- src/main/java/kodlamaio/hrms/business/concretes/EmployerManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/EmployerManager.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.EmployerService;
+import kodlamaio.hrms.core.utilities.helpers.email.EmailService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.EmployerDao;
 import kodlamaio.hrms.entities.concretes.Employer;
 
@@ -13,16 +18,23 @@ import kodlamaio.hrms.entities.concretes.Employer;
 public class EmployerManager implements EmployerService {
 
 	private EmployerDao employerDao;
+	private EmailService emailService;
 	
 	@Autowired
-	public EmployerManager(EmployerDao employerDao) {
-		super();
+	public EmployerManager(EmployerDao employerDao,EmailService emailService) {
 		this.employerDao = employerDao;
+		this.emailService=emailService;
 	}
 
 	@Override
-	public List<Employer> getAll() {
-		return this.employerDao.findAll();
+	public DataResult<List<Employer>> getAll() {
+		return new SuccessDataResult<List<Employer>>(employerDao.findAll(), "Veriler başarıyla listelendi.");
 	}
 
+	@Override
+	public Result register(Employer employer) {
+		employerDao.save(employer);
+		emailService.sendEmail("Doğrulama işlemi başarılı bir şekilde gerçekleştirildi.");
+		return new SuccessResult("Kullanıcı başarılı bir şekilde kayıt oldu. Doğrulama işlemi için email adresinizi kontrol ediniz");
+	}
 }
