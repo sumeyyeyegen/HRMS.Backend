111e4ea86d9f2e2e517e856ae5d9d60628f22dcc
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-29 20:26:17 +0300

    City and jobAdvertisement entity classes added.

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/City.java
@@ -0,0 +1,35 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="cities")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","JobAdvertisement"})
+
+public class City {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", nullable = false)
+	private int id;
+	@Column(name="name", nullable = false, length = 50)
+	private String name;
+	@OneToMany(mappedBy="city")
+	private List<JobAdvertisement> jobAdvertisement;
+}
---
+++ src/main/java/kodlamaio/hrms/entities/concretes/JobAdvertisement.java
@@ -0,0 +1,62 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="job_advertisements")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class JobAdvertisement {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id", nullable = false)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="employer_id", nullable = false)
+	private Employer employer;
+	
+	@ManyToOne
+	@JoinColumn(name = "city_id", nullable = false)
+	private City city;
+	
+	@ManyToOne
+	@JoinColumn(name = "job_id", nullable = false)
+	private Job job;
+	
+	@Column(name="min_salary")
+	private double minSalary;
+	
+	@Column(name="max_salary")
+	private double maxSalary;
+	
+	@Column(name="job_description", nullable = false, length = 500)
+	private String jobDescription;
+	
+	@Column(name="open_positions", nullable = false)
+	private int openPositions;
+	
+	@Column(name="application_deadline", nullable = false, length = 50)
+	private String applicationDeadline;
+	
+	@Column(name="is_active", nullable = false)
+	private boolean isActive;
+	
+	@Column(name="release_date", nullable = false, length = 50)
+	private String releaseDate;
+	
+}
