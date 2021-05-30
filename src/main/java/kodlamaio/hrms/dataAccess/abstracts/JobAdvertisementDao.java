1574d6e75b9c5a137a0f1f0f98d2be49ab19f2b9
sumeyye <sumeyyeyegen746@gmail.com>
2021-05-30 18:36:36 +0300

    JobAdvertisementDao jobadvertisements listed.

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdvertisementDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdvertisementDao.java
@@ -3,9 +3,13 @@ package kodlamaio.hrms.dataAccess.abstracts;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import kodlamaio.hrms.entities.concretes.JobAdvertisement;
 
+
 public interface JobAdvertisementDao extends JpaRepository<JobAdvertisement, Integer>{
-	
+	@Query("SELECT new hrms.entities.concretes.JobAdvertisement(emp.companyName,j.title,ja.openPositions,ja.releaseDate,ja.applicationDeadline) FROM JobAdvertisement ja JOIN ja.employer e JOIN ja.job j WHERE ja.isActive=:isActive AND e.companyName=:companyName")                             
+	List<JobAdvertisement> getByIsActiveForList(@Param("isActive") boolean isActive);
 }
