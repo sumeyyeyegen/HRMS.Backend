2ebfcd12c6a1bb647b5ea0d7d8834c3c833dea02
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-01 23:05:14 +0300

    JobAdvertisementForListDto created and necessary definitions were made.

---
+++ src/main/java/kodlamaio/hrms/entities/dtos/JobAdvertisementForListDto.java
@@ -0,0 +1,45 @@
+package kodlamaio.hrms.entities.dtos;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+
+public class JobAdvertisementForListDto {
+	@NotBlank
+	@Size(max = 255, min = 0)
+	private String companyName;
+	
+	@NotBlank
+	@Size(max = 255, min = 0)
+	private String title;
+	
+	@Positive
+	private int openPositions;
+	
+	@Past
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date releaseDate;
+	
+	@Future
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date applicationDeadline;
+}
