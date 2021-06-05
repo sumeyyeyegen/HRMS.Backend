b031f30ebc02065734ca2f5d184479a4e8cdd9e7
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-05 03:00:37 +0300

    base service added.

---
+++ src/main/java/kodlamaio/hrms/core/business/BaseService.java
@@ -0,0 +1,15 @@
+package kodlamaio.hrms.core.business;
+
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.JobAdvertisement;
+
+public interface BaseService<T,Id> {
+	Result add(T entity);
+	Result delete(T entity);
+	DataResult<List<T>> getAll();
+	DataResult<T> getById(Id id);
+	Result update(T entity);
+}
