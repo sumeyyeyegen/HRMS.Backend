80d8d98f24cf72152d8c4bc6f66651bab2697726
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-05 17:24:25 +0300

    Added necessary entity classes for candidate cv.

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/CandidateCv.java
@@ -0,0 +1,45 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="candidate_cvs")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CandidateCv {
+	
+	@NotNull
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@OneToOne
+	@NotNull
+	@JoinColumn(name="candidate_id")
+	private Candidate candidate;
+	
+	@NotNull
+	@Column(name="cover_letter", length = 255 )
+	private String coverLetter;
+	
+	@NotNull
+	@Column(name = "created_at", columnDefinition = "Date default CURRENT_DATE")
+	private final LocalDateTime createdAt = LocalDateTime.now();
+}
