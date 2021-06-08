a45d81f27d527be9b6aac0afc3949ef300ba9a16
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-09 02:02:40 +0300

    employeeConfirm and employeeConrifmEmployer created and veriables are defined.

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/EmployeeConfirm.java
@@ -0,0 +1,50 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="employee_confirms")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","EmployeeConfirm"})
+@Inheritance(strategy=InheritanceType.JOINED)
+
+public class EmployeeConfirm {
+	
+	@Id
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@NotNull
+	@Column(name="is_confirmed")
+	private boolean isConfirmed;
+	
+	@Column(name="confirm_date")
+	private LocalDateTime confirmDate;	
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name="employee_id")
+	private Employee employee;
+}
---
+++ src/main/java/kodlamaio/hrms/entities/concretes/EmployeeConfirmEmployer.java
@@ -0,0 +1,43 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="employee_confirm_employers")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","EmployeeConfirmEmployer"})
+@EqualsAndHashCode(callSuper=false)
+
+public class EmployeeConfirmEmployer extends EmployeeConfirm {
+	
+	@Id
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@NotNull
+	@JsonIgnore()
+	@OneToOne
+	@JoinColumn(name="employer_id")
+	private Employer employer;
+	
+}
