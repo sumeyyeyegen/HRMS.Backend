e19971c536e819290f3fcd4eeb5da8dcac003eb0
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-09 02:08:38 +0300

    EmployeeConfirmEmployerService extends BaseService Interface.

--- src/main/java/kodlamaio/hrms/business/abstracts/EmployeeConfirmEmployerService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/EmployeeConfirmEmployerService.java
@@ -1,5 +1,9 @@
 package kodlamaio.hrms.business.abstracts;
 
-public interface EmployeeConfirmEmployerService {
+import kodlamaio.hrms.core.business.BaseService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.entities.concretes.EmployeeConfirmEmployer;
 
+public interface EmployeeConfirmEmployerService extends BaseService<EmployeeConfirmEmployer, Integer> {
+	DataResult<EmployeeConfirmEmployer> getByUserId(final int userId);
 }
