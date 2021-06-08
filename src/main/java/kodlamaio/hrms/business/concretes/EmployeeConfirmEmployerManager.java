bf3a0a490b43beae7dfb6d0afed8baf5a87cb7b2
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-09 02:12:40 +0300

    functions in EmployeeConfirmEmployerService interface are overridden.

--- src/main/java/kodlamaio/hrms/business/concretes/EmployeeConfirmEmployerManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/EmployeeConfirmEmployerManager.java
@@ -1,7 +1,48 @@
 package kodlamaio.hrms.business.concretes;
 
+import java.util.List;
+
 import kodlamaio.hrms.business.abstracts.EmployeeConfirmEmployerService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.EmployeeConfirmEmployer;
 
 public class EmployeeConfirmEmployerManager implements EmployeeConfirmEmployerService {
+
+	@Override
+	public Result add(EmployeeConfirmEmployer entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result delete(EmployeeConfirmEmployer entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<List<EmployeeConfirmEmployer>> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<EmployeeConfirmEmployer> getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result update(EmployeeConfirmEmployer entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataResult<EmployeeConfirmEmployer> getByUserId(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
