25318815b1e3bfbf41a1e87f680342f637c890f2
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-09 02:08:56 +0300

    BaseService imported.

--- src/main/java/kodlamaio/hrms/business/abstracts/EmployerService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/EmployerService.java
@@ -1,7 +1,7 @@
 package kodlamaio.hrms.business.abstracts;
-
 import java.util.List;
 
+import kodlamaio.hrms.core.business.BaseService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Employer;
