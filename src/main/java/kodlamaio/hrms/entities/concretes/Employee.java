307f5cbe6684acd7f2dc3d2313eedb2d3f8793e6
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-09 02:05:33 +0300

    employer and employee entity classes are updated.

--- src/main/java/kodlamaio/hrms/entities/concretes/Employee.java
+++ src/main/java/kodlamaio/hrms/entities/concretes/Employee.java
@@ -1,15 +1,19 @@
 package kodlamaio.hrms.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import kodlamaio.hrms.core.entities.User;
 import lombok.AllArgsConstructor;
@@ -22,17 +26,29 @@ import lombok.NoArgsConstructor;
 @Table(name="employees")
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","Employee"})
 @EqualsAndHashCode(callSuper = false)
+
 public class Employee extends User {	
+	
 	@Id
-	@Column(name="id")
+	@NotNull
 	@JsonIgnore()
 	@PrimaryKeyJoinColumn(name="id",referencedColumnName = "id")
 	private int id;
 
-	@Column(name="first_name",nullable = false,length = 35)
-	private String first_name;
+	@NotNull
+	@Length(max = 35)
+	@Column(name="first_name")
+	private String firstName;
 	
-	@Column(name="last_name",nullable = false,length = 35)
-	private String last_name;
+	@NotNull
+	@Length(max = 35)
+	@Column(name="last_name")
+	private String lastName;
+	
+	@NotNull
+	@JsonIgnore()
+	@OneToMany(mappedBy = "employee")
+	private List<EmployeeConfirm> employeeConfirms ;
 }
--- src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
+++ src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
@@ -1,17 +1,19 @@
 package kodlamaio.hrms.entities.concretes;
 
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import kodlamaio.hrms.core.entities.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,26 +25,41 @@ import lombok.NoArgsConstructor;
 @Table(name="employers")
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties({"hibernateLazyInitializer","handler","JobAdvertisement"})
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","Employer"})
 @EqualsAndHashCode(callSuper=false)
+
 public class Employer extends User {
+	
 	@Id
+	@NotNull
+	@JsonIgnore()
 	@Column(name="id")
-	@JsonIgnore
 	@PrimaryKeyJoinColumn(name="id",referencedColumnName = "id")
 	private int id;
 	
-	@Column(name="company_name",nullable = false,length = 255)
+	@NotNull
+	@Length(max = 255)
+	@Column(name="company_name")
 	private String companyName;
 	
-	@Column(name="web_site",nullable = false,length = 50)
+	@NotNull
+	@Length(max = 50)
+	@Column(name="web_site")
 	private String webSite;
 	
-	@Column(name="phone_number",nullable = false,length = 12)
+	@NotNull
+	@Length(max = 12)
+	@Column(name="phone_number")
 	private String phoneNumber;
 	
+	@NotNull
 	@JsonIgnore()
 	@OneToMany(mappedBy = "employer")
 	private List<JobAdvertisement> jobAdvertisements;
 	
+	@NotNull
+	@JsonIgnore()
+	@OneToOne(mappedBy = "employer")
+	private VerificationCodeEmployer verificationCodeEmployer;
+		
 }
