79b7c8a6c2ff476d283f912bb822883b90001c89
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-20 03:24:21 +0300

    manager to service.

--- src/main/java/kodlamaio/hrms/api/controllers/CandidateCvsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/CandidateCvsController.java
@@ -9,26 +9,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kodlamaio.hrms.business.abstracts.CandidateCvService;
 import kodlamaio.hrms.business.concretes.CandidateCvManager;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.CandidateCv;
 import kodlamaio.hrms.entities.concretes.CandidateCvEducation;
+import kodlamaio.hrms.entities.dtos.CandidateCvListDto;
 
 @RestController
 @RequestMapping("/api/candidate/cvs")
 public class CandidateCvsController {
-	private CandidateCvManager candidateCvManager;
+	private CandidateCvService candidateCvService;
 
 	@Autowired
-	public CandidateCvsController(CandidateCvManager candidateCvManager) {
+	public CandidateCvsController(CandidateCvService candidateCvService) {
 		super();
-		this.candidateCvManager = candidateCvManager;
+		this.candidateCvService = candidateCvService;
 	}
 	
 	@GetMapping("/getbycandidatecvid")
-	public ResponseEntity<DataResult<CandidateCv>> getByCandidateCv_Id(int id){
-		DataResult<CandidateCv> result = candidateCvManager.getById(id);
+	public ResponseEntity<DataResult<CandidateCv>> getByCandidateCvId(int candidateCvId){
+		DataResult<CandidateCv> result = candidateCvService.getById(candidateCvId) ;
 
 		if (!result.isSuccess())
 			return new ResponseEntity<DataResult<CandidateCv>>(result, HttpStatus.BAD_REQUEST);
