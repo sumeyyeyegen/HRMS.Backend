714a455314b5914561020989a59cb463a322d1b7
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-20 03:26:31 +0300

    updated candidateCvController.

--- src/main/java/kodlamaio/hrms/api/controllers/CandidateCvsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/CandidateCvsController.java
@@ -3,19 +3,13 @@ package kodlamaio.hrms.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.CandidateCvService;
-import kodlamaio.hrms.business.concretes.CandidateCvManager;
 import kodlamaio.hrms.core.utilities.results.DataResult;
-import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.CandidateCv;
-import kodlamaio.hrms.entities.concretes.CandidateCvEducation;
-import kodlamaio.hrms.entities.dtos.CandidateCvListDto;
 
 @RestController
 @RequestMapping("/api/candidate/cvs")
