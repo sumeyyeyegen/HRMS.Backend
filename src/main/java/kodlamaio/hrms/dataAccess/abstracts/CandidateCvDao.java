ad65dd1d64eb411a9539bce58afd4a240ba3e577
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-27 19:22:24 +0300

    candidateCvDao changed.

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/CandidateCvDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/CandidateCvDao.java
@@ -1,11 +1,11 @@
 package kodlamaio.hrms.dataAccess.abstracts;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import kodlamaio.hrms.entities.concretes.CandidateCv;
 
 public interface CandidateCvDao extends JpaRepository<CandidateCv, Integer> {
-	Optional<CandidateCv> getById(int id);
-	
+	List<CandidateCv> getAllByCandidate_Id(int candidateId);
 }
