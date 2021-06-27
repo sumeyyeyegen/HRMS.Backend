b4e28fc64e455934b9853f4a55273b6f37940e90
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-27 19:21:58 +0300

    JobAdvertisementDao updated.

--- src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdvertisementDao.java
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobAdvertisementDao.java
@@ -10,20 +10,20 @@ import kodlamaio.hrms.entities.dtos.JobAdvertisementForListDto;
 
 public interface JobAdvertisementDao extends JpaRepository<JobAdvertisement, Integer>{
 	
-	@Query("Select new kodlamaio.hrms.entities.dtos.JobAdvertisementForListDto"
-			+ "(e.companyName , job.title , j.openPositions, j.releaseDate, j.applicationDeadline) "
-			+ "From JobAdvertisement j JOIN j.employer e JOIN j.job job WHERE j.isActive = true")
-	List<JobAdvertisementForListDto> getByIsActiveTrue();
+//	@Query("Select new kodlamaio.hrms.entities.dtos.JobAdvertisementForListDto"
+//			+ "(e.companyName , job.title , j.openPositions, j.releaseDate, j.applicationDeadline) "
+//			+ "From JobAdvertisement j JOIN j.employer e JOIN j.job job WHERE j.isActive = true")
+	List<JobAdvertisement> getByIsActiveTrue();
 	
 	@Query("Select new kodlamaio.hrms.entities.dtos.JobAdvertisementForListDto"
 			+ "(e.companyName , job.title , j.openPositions, j.releaseDate, j.applicationDeadline) "
 			+ "From JobAdvertisement j JOIN j.employer e JOIN j.job job WHERE j.isActive = true ORDER BY j.releaseDate DESC")
 	List<JobAdvertisementForListDto> getByIsActiveTrueOrderByReleaseDateDesc();
 	
-	@Query("Select new kodlamaio.hrms.entities.dtos.JobAdvertisementForListDto"
-			+ "(e.companyName , job.title , j.openPositions, j.releaseDate, j.applicationDeadline) "
-			+ "From JobAdvertisement j JOIN j.employer e JOIN j.job job WHERE j.isActive = true ORDER BY j.releaseDate ASC")
-	List<JobAdvertisementForListDto> getByIsActiveTrueOrderByReleaseDateAsc();
+//	@Query("Select new kodlamaio.hrms.entities.dtos.JobAdvertisementForListDto"
+//			+ "(e.companyName , job.title , j.openPositions, j.releaseDate, j.applicationDeadline) "
+//			+ "From JobAdvertisement j JOIN j.employer e JOIN j.job job WHERE j.isActive = true ORDER BY j.releaseDate ASC")
+	List<JobAdvertisement> getByIsActiveTrueOrderByReleaseDateAsc();
 	
 	@Query("Select new kodlamaio.hrms.entities.dtos.JobAdvertisementForListDto"
 			+ "(e.companyName , job.title , j.openPositions, j.releaseDate, j.applicationDeadline) "
