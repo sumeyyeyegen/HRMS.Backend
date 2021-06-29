67424ca65ef85a0bb66891830939c20f0ba45708
sumeyye <sumeyyeyegen746@gmail.com>
2021-06-29 05:05:06 +0300

    WorkTime table added.

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/WorkTime.java
@@ -0,0 +1,45 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="work_times")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class WorkTime {
+
+	@Id
+	@NotNull
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	@Length(max = 30)
+	@Column(name="name")
+	private String name;
+	
+	@NotNull
+	@JsonIgnore()
+	@OneToMany(mappedBy="workTime")
+	private List<JobAdvertisement> jobAdvertisements;
+}
