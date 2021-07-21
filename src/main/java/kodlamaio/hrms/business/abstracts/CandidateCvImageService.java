883f776620aa545015c36942259ed1310697f247
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-21 03:11:29 +0300

    imageService updated.

--- src/main/java/kodlamaio/hrms/business/abstracts/CandidateCvImageService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/CandidateCvImageService.java
@@ -6,8 +6,10 @@ import kodlamaio.hrms.core.business.BaseService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.CandidateCvImage;
+import kodlamaio.hrms.entities.dtos.CvImageForAddDto;
 
 public interface CandidateCvImageService extends BaseService<CandidateCvImage, Integer>{
-	Result add(CandidateCvImage candidateCvImage, MultipartFile file);
+	Result add(CvImageForAddDto imageDto, MultipartFile file);
 	DataResult<CandidateCvImage> getByCandidateCvId(int candidateCvId);
+	Result addImage(CvImageForAddDto imageDto);
 }
