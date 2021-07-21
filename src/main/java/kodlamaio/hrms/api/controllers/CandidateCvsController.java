62d9538a513b5a6bc26ac672d7a75266c86039a7
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-21 03:07:32 +0300

    add and update functions added to CandidateCvController.

--- src/main/java/kodlamaio/hrms/api/controllers/CandidateCvsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/CandidateCvsController.java
@@ -7,12 +7,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.CandidateCvService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.ErrorDataResult;
+import kodlamaio.hrms.core.utilities.results.ErrorResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.entities.concretes.CandidateCv;
+import kodlamaio.hrms.entities.dtos.CvDto;
+import kodlamaio.hrms.entities.dtos.CvForUpdateAndAddDto;
 
 @CrossOrigin()
 @RestController
@@ -24,6 +34,15 @@ public class CandidateCvsController {
 	public CandidateCvsController(CandidateCvService candidateCvService) {
 		super();
 		this.candidateCvService = candidateCvService;
+
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody CvDto cvDto){
+		Result result = candidateCvService.addCv(cvDto);
+		if(!result.isSuccess())
+			return new ErrorResult("İşlem başarılı değil.");
+		return new SuccessResult("Cv başarılı bir şekilde eklendi");
 	}
 	
 	@GetMapping("/getall")
@@ -36,9 +55,19 @@ public class CandidateCvsController {
 		return ResponseEntity.ok(result);
 	}
 	
+	@PutMapping("/update") 
+	public ResponseEntity<Result> updateCv(@RequestBody CvDto cvDto){
+		Result result = candidateCvService.updateCv(cvDto);
+
+		if (!result.isSuccess())
+			return new ResponseEntity<Result>(result, HttpStatus.BAD_REQUEST);
+
+		return ResponseEntity.ok(result);
+	}
+	 
 	
 	@GetMapping("/getbycandidatecvid")
-	public ResponseEntity<DataResult<CandidateCv>> getByCandidateCvId(int candidateCvId){
+	public ResponseEntity<DataResult<CandidateCv>> getByCandidateCvId(@RequestParam int candidateCvId){
 		DataResult<CandidateCv> result = candidateCvService.getById(candidateCvId) ;
 
 		if (!result.isSuccess())
@@ -48,9 +77,9 @@ public class CandidateCvsController {
 	}
 	
 	@GetMapping("/getallbycandidateid")
-	public ResponseEntity<DataResult<List<CandidateCv>>> getAllByCandidateId(int candidateCvId){
-		DataResult<List<CandidateCv>> result = candidateCvService.getAllByCandidateId(candidateCvId);
-
+	public ResponseEntity<DataResult<List<CandidateCv>>> getAllByCandidateId(@RequestParam int candidateId){
+		DataResult<List<CandidateCv>> result = candidateCvService.getAllByCandidateId(candidateId);
+ 
 		if (!result.isSuccess())
 			return new ResponseEntity<DataResult<List<CandidateCv>>>(result, HttpStatus.BAD_REQUEST);
 
