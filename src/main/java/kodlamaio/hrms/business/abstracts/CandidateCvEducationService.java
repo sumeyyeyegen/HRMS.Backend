20524d1718041037741cfb7804f8c2093fde7a18
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-21 03:09:29 +0300

    addEducation function added to cvEducationService.

--- src/main/java/kodlamaio/hrms/business/abstracts/CandidateCvEducationService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/CandidateCvEducationService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import kodlamaio.hrms.core.business.BaseService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.CandidateCvEducation;
+import kodlamaio.hrms.entities.dtos.CvEducationForAddDto;
 
 public interface CandidateCvEducationService extends BaseService<CandidateCvEducation, Integer> {
 	DataResult<List<CandidateCvEducation>> getByCandidateCvEducations_Id(int id); 
 	DataResult<List<CandidateCvEducation>> getAllByCandidateCvIdOrderByGraduationDateDesc(int candidateCvId);
 	DataResult<List<CandidateCvEducation>> getAllByCandidateCvIdOrderByGraduationDateAsc(int candidateCvId);
+	Result addEducation(CvEducationForAddDto eduDto);
 }
