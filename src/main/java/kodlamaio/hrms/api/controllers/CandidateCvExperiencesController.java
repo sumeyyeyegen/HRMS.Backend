6d4e86c997fe37ac53cb311266c8540e01b6d3b1
sumeyye <sumeyyeyegen746@gmail.com>
2021-07-23 15:40:15 +0300

    update function added to CandidateCvExperienceController.

--- src/main/java/kodlamaio/hrms/api/controllers/CandidateCvExperiencesController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/CandidateCvExperiencesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,6 +37,13 @@ public class CandidateCvExperiencesController {
 	@PostMapping("/add")
 	public ResponseEntity<Result> add(@RequestBody CvExperienceForAddAndUpdateDto cvExperiencesForAddDto) {
 		final Result result = candidateCvExperienceService.addExperiences(cvExperiencesForAddDto);
+
+		return ResponseEntity.ok(result);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Result> update(@RequestBody CvExperienceForAddAndUpdateDto cvExperiencesForAddDto) {
+		final Result result = candidateCvExperienceService.updateExperience(cvExperiencesForAddDto);
 
 		return ResponseEntity.ok(result);
 	}
